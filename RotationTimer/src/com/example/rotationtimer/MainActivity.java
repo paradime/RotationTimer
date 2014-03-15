50901043ecb5d982c615ab74b3eaaea4081199ae
paradime <bryonwilkins94@gmail.com>
2014-03-14 23:33:40 -0400

    Added myCountDownTimer to keep run timer operations

--- RotationTimer/src/com/example/rotationtimer/MainActivity.java
+++ RotationTimer/src/com/example/rotationtimer/MainActivity.java
@@ -13,57 +13,52 @@ import android.os.CountDownTimer;
 
 public class MainActivity extends Activity implements OnClickListener {
 
-  long mMilliseconds = 60000;
+  long mMilliseconds = 6000;
+  long defaultTime = 0;
   SimpleDateFormat mSimpleDateFormat = new SimpleDateFormat("HH:mm:ss");
   TextView mTextView;
   long s1;
-
-  CountDownTimer mCountDownTimer = new CountDownTimer(mMilliseconds, 1000) {
-    @Override
-    public void onFinish() {
-      mTextView.setText(mSimpleDateFormat.format(0));
-    }
-
-    public void onTick(long millisUntilFinished) {
-      mTextView.setText(mSimpleDateFormat.format(millisUntilFinished));
-      s1 = millisUntilFinished;
-    }
-  };
+  myCountDownTimer mCountDownTimer;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    mSimpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+    mCountDownTimer = new myCountDownTimer(mMilliseconds, 1000, this);
     mTextView = (TextView) findViewById(R.id.countDownTimer);
     ((Button) findViewById(R.id.start_button)).setOnClickListener(this);
     ((Button) findViewById(R.id.stop_button)).setOnClickListener(this);
+    ((TextView) findViewById(R.id.countDownTimer)).setOnClickListener(this);
+    update();
+  }
 
+  public void update() {
+    mTextView.setText(mCountDownTimer.getFormat());
+  }
+
+  public void newClock(long curTime) {
+    mCountDownTimer = new myCountDownTimer(curTime, 1000, this);
     mCountDownTimer.start();
+  }
 
+  public void newClock() {
+    mCountDownTimer = new myCountDownTimer(defaultTime, 1000, this);
+    mCountDownTimer.start();
   }
 
   @Override
   public void onClick(View v) {
     switch (v.getId()) {
     case R.id.start_button:
-      mCountDownTimer = new CountDownTimer(s1, 1000) {
-        @Override
-        public void onFinish() {
-          mTextView.setText(mSimpleDateFormat.format(0));
-        }
-
-        public void onTick(long millisUntilFinished) {
-          mTextView.setText(mSimpleDateFormat.format(millisUntilFinished));
-          s1 = millisUntilFinished;
-        }
-      };
-      mCountDownTimer.start();
+      mCountDownTimer.startAgain();
       break;
     case R.id.stop_button:
       mCountDownTimer.cancel();
       break;
+    }
+    case R.id.countDownTimer{
+      
     }
   }
 }
---
+++ RotationTimer/src/com/example/rotationtimer/myCountDownTimer.java
@@ -0,0 +1,49 @@
+package com.example.rotationtimer;
+
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
+import android.app.Activity;
+import android.os.CountDownTimer;
+
+public class myCountDownTimer extends CountDownTimer {
+
+  SimpleDateFormat mSimpleDateFormat;
+  long timeLeft, defaultTime;
+  MainActivity myActivity;
+
+  public myCountDownTimer(long millisInFuture, long countDownInterval,
+      MainActivity curView) {
+    super(millisInFuture, countDownInterval);
+    timeLeft = millisInFuture;
+    myActivity = curView;
+    mSimpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+    mSimpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+  }
+
+  public myCountDownTimer(long millisInFuture, long countDownInterval) {
+    super(millisInFuture, countDownInterval);
+  }
+
+  public String getFormat() {
+    return mSimpleDateFormat.format(timeLeft);
+  }
+
+  @Override
+  public void onFinish() {
+    myActivity.newClock();
+
+  }
+
+  public void startAgain() {
+    myActivity.newClock(timeLeft);
+  }
+
+  @Override
+  public void onTick(long millisUntilFinished) {
+    timeLeft = millisUntilFinished;
+    myActivity.update();
+
+  }
+
+}
